b25d22aef6dbfa2e40cf2f465e012194205ac714
Carlos Mañas <carlos@spacenomads.com>
2019-02-13 14:38:18 +0100

    Use a modern solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route, NavLink, Link} from 'react-router-dom';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header className="app__header">
+          <nav className="header__nav">
+            <ul className="menu">
+              <li className="menu__item">
+                <NavLink exact to="/" className="menu__link" activeClassName="is-active">Home</NavLink>
+              </li>
+              <li className="menu__item">
+                <NavLink to="/blog" className="menu__link" activeClassName="is-active">Blog</NavLink>
+              </li>
+            </ul>
+          </nav>
         </header>
+        
+        <main className="app__main">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/blog" component={Blog} />
+          </Switch>
+        </main>
       </div>
     );
   }
